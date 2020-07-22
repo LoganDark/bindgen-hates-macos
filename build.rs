547773ab177f93cbb77cbbff5d54ac9e53176a06
Logan Darklock <logandarklock@gmail.com>
2020-07-21 22:47:33 -0700

    use clang_arg for even more minimal example

--- build.rs
+++ build.rs
@@ -3,23 +3,13 @@ use std::path::PathBuf;
 fn main() {
 	println!("cargo:rerun-if-changed=wrapper.hpp");
 
-	let cxx_flags = String::from("-std=c++14");
-
-	// It's apparently impossible to define this programmatically, so I have to
-	// abuse this environment variable meant to be used by the user!
-	std::env::set_var(
-		"BINDGEN_EXTRA_CLANG_ARGS",
-		std::env::var("BINDGEN_EXTRA_CLANG_ARGS")
-			.map(|str| str + " " + cxx_flags.as_ref())
-			.unwrap_or(cxx_flags)
-	);
-
 	std::env::set_var("RUST_LOG", "bindgen");
 	env_logger::init();
 
 	let bindings = bindgen::Builder::default()
 		.header("wrapper.hpp")
 		.detect_include_paths(false)
+		.clang_arg("-std=c++14")
 		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
 		.generate()
 		.expect("Couldn't make bindings!");
